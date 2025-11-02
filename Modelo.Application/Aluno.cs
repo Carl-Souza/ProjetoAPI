8a624536009826d8fd00af8c7ced057c314a53aa
Carl <carlsagan77@gmail.com>
2025-11-02 20:56:44 -0300

    Adicionar arquivos de projeto.

---
+++ Modelo.Application/Aluno.cs
@@ -0,0 +1,18 @@
+﻿namespace Modelo.Application
+{
+    public class Aluno
+    {
+        public int Codigo { get; set; }
+        public string Matricula { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public string CEP { get; set; }
+        public string Endereco { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Numero { get; set; }
+        public DateOnly DataNascimento { get; set; }
+
+    }
+}
